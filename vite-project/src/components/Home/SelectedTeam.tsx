26a7cbef15463ef5e799c486a456dec591f06c1a
lilyplw <clown.b.c@gmail.com>
2023-08-08 16:22:00 +0900

    [update] Type bug fixes

--- vite-project/src/components/Home/SelectedTeam.tsx
+++ vite-project/src/components/Home/SelectedTeam.tsx
@@ -6,8 +6,11 @@ const SelectedTeam = () => {
 	const { selectedTeams } = useContext(AppContext);
 	return (
 		<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 gap-6">
-			{selectedTeams.map((selected) => {
-				return <TeamCard name={selected.name} icon={selected.icon} />;
+			{selectedTeams.map((selected, index) => {
+				if (!selected.name || !selected.icon) return null; // nameやiconがundefinedの場合は何もレンダリングしない
+				return (
+					<TeamCard key={index} name={selected.name} icon={selected.icon} />
+				);
 			})}
 		</div>
 	);
